e47ef7bae4f8617835054439b6c4015a4faeb642
dkdud9261 <ayoganatanatda@naver.com>
2020-12-17 00:23:24 +0900

    temporary

--- app/src/main/java/com/example/booktradeapp/BookList.java
+++ app/src/main/java/com/example/booktradeapp/BookList.java
@@ -1,6 +1,9 @@
 package com.example.booktradeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
@@ -21,12 +24,19 @@ public class BookList extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.list_book);
         BookAdapter adapter = new BookAdapter(this, books);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(BookList.this, Show.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void InitializeBookData() {
         books = new ArrayList<BookData>();
 
-        books.add(new BookData(R.drawable.ic_launcher_foreground, "제목1", "10000원"));
+        books.add(new BookData(R.drawable.calculus, "Calculus 8th edition", "25000원"));
         books.add(new BookData(R.drawable.ic_launcher_foreground, "제목2", "10000원"));
         books.add(new BookData(R.drawable.ic_launcher_foreground, "제목3", "10000원"));
         books.add(new BookData(R.drawable.ic_launcher_foreground, "제목4", "10000원"));
