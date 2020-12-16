80d75436dec4c72bde7c3f1d5137621e0e22a874
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2020-12-17 06:30:09 +0900

    connect complete to list

--- app/src/main/java/com/example/booktradeapp/Writeinfo.java
+++ app/src/main/java/com/example/booktradeapp/Writeinfo.java
@@ -1,5 +1,6 @@
 package com.example.booktradeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 //import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,16 +10,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class Writeinfo extends AppCompatActivity {
+    Button check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writeinfo);
 
+        check = (Button)findViewById(R.id.completewrite);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Writeinfo.this, BookList.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
