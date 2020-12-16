203e7215a2ff2fb9d93340816b340811efb3a10e
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2020-12-17 07:16:30 +0900

    turn camera

--- app/src/main/java/com/example/booktradeapp/Writeinfo.java
+++ app/src/main/java/com/example/booktradeapp/Writeinfo.java
@@ -9,20 +9,24 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class Writeinfo extends AppCompatActivity {
     Button check;
+    Button pic = null;
+    ImageView iv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writeinfo);
-
+        setup();
         check = (Button)findViewById(R.id.completewrite);
         check.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +36,23 @@ public class Writeinfo extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void setup()
+    {
+        pic = (Button)findViewById(R.id.getpicture);
+        iv = (ImageView)findViewById(R.id.bookimage);
+        pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        iv.setImageURI(data.getData());
     }
 }
